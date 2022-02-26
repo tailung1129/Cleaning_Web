8dae2c16735df87015f5e321db27423f2c2b0e7b
smile1103 <95035423+smile1103@users.noreply.github.com>
2022-02-26 19:57:50 +0900

    redux_fix

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux"
+import store from "./redux/store"
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootreducer from './reducer';
+
+
+const initialstate={};
+
+const middleware = [thunk];
+
+const store = createStore(rootreducer,initialstate,composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store;
