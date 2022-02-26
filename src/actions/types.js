3ec0eb102e6de4e7cad594fa65343e104615f4bc
smile1103 <95035423+smile1103@users.noreply.github.com>
2022-02-26 16:23:09 +0900

    redux_fix

--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_POSTS = "FETCH_POSTS"
-export const NEW_POST = "NEW_POST"
-export const POST_LOGIN = "POST_LOGIN"
-export const GET_REQUEST = "GET_REQUEST"
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import postReducer from './postReducer.js'
-import requestReducer from "./requestReducer.js";
-
-export default combineReducers({
-    posts:postReducer,
-    requests:requestReducer
-});
